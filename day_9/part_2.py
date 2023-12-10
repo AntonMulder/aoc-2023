0d9b440e2adda711d77fb4d91e5aca031ea79fe0
Anton Mulder <anton.mulder@wearespindle.com>
2023-12-10 12:45:00 +0100

    Day 9

---
+++ day_9/part_2.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+from utils.io import readlines
+
+
+def run(puzzle_input):
+    answer = 0
+
+    sequences = ([int(y) for y in x.split(" ")] for x in puzzle_input)
+
+    def _get_differences(x: list[int]) -> list[int]:
+        return [x[i] - x[i - 1] for i in range(1, len(x))]
+
+    for sequence in sequences:
+        difference = _get_differences(sequence)
+        sub_sequences = [sequence, difference]
+
+        while not all(x == 0 for x in difference):
+            new_difference = _get_differences(difference)
+            sub_sequences.append(new_difference)
+            difference = new_difference
+
+        answer += reduce(lambda acc, x: x[0] - acc, reversed(sub_sequences), 0)
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(run(readlines("data/puzzle_input.txt")))
